8196c3fde569102b2a38e109b2b1353640d6a6b5
mateolarreaferro <55174368+mateolarreaferro@users.noreply.github.com>
2021-10-22 13:24:10 -0400

    Basic Layout for RTWSP

--- VR-Ready/Assets/Mateo/Scripts/Executable Poetry/LogicTable.cs
+++ VR-Ready/Assets/Mateo/Scripts/Executable Poetry/LogicTable.cs
@@ -4,38 +4,175 @@ using UnityEngine;
 
 public class LogicTable : MonoBehaviour
 {
-    [SerializeField] GameObject[] _words;
-    [SerializeField] bool _word1, _word2, _word3, _word4, _word5;
+    
+    //Which word should be playing
+    public bool[] Dominant = new bool[6];
+
+    //Quality (for FFT)
+    public bool _negative, _neutral, _positive;
+    [SerializeField] Material LEDMaterial = null;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        _word1 = _word2 = _word3 = _word4 = _word5 = false;
         
+        //Init Dominants
+        for (int i = 0; i < Dominant.Length; i++)
+        {
+            Dominant[i] = false;
+        }
+
+        //Default Material for LED system
+        LEDMaterial.SetColor("_ColorLED", Color.grey);
+
+
     }
+
+
+
+    private void Update()
+    {
+        if (_negative)
+        {
+            LEDMaterial.SetColor("_ColorLED", Color.blue);
+        }
+        else if (_neutral)
+        {
+            LEDMaterial.SetColor("_ColorLED", Color.gray);
+        }
+        else if (_positive)
+        {
+            LEDMaterial.SetColor("_ColorLED", Color.green);
+        }
+        else
+        {
+            LEDMaterial.SetColor("_ColorLED", Color.grey);
+        }
+    }
+
+
+
+
+
+    // ENTER THE TABLE
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("1"))
+        if (other.gameObject.CompareTag("amnesia"))
         {
-            _word1 = true;
+           for (int i = 0; i < Dominant.Length; i++)
+            {
+                Dominant[i] = false;
+            }
+
+            Dominant[0] = true;
+            _negative = true;
+
+            _neutral = _positive = false;
+
         }
-        else if (other.gameObject.CompareTag("2"))
+        else if (other.gameObject.CompareTag("immortal"))
         {
-            _word2 = true;
+            for (int i = 0; i < Dominant.Length; i++)
+            {
+                Dominant[i] = false;
+            }
+
+            Dominant[1] = true;
+            _positive = true;
+
+            _neutral = _negative = false;
         }
-        else if (other.gameObject.CompareTag("3"))
+        else if (other.gameObject.CompareTag("trapped"))
         {
-            _word3 = true;
+            for (int i = 0; i < Dominant.Length; i++)
+            {
+                Dominant[i] = false;
+            }
+
+            Dominant[2] = true;
+            _negative = true;
+
+            _neutral = _positive = false;
         }
-        else if (other.gameObject.CompareTag("4"))
+        else if (other.gameObject.CompareTag("orthogonal"))
         {
-            _word4 = true;
+            for (int i = 0; i < Dominant.Length; i++)
+            {
+                Dominant[i] = false;
+            }
+
+            Dominant[3] = true;
+            _neutral = true;
+
+            _negative = _positive = false;
         }
-        else if (other.gameObject.CompareTag("5"))
+        else if (other.gameObject.CompareTag("enlightened"))
         {
-            _word5 = true;
+            for (int i = 0; i < Dominant.Length; i++)
+            {
+                Dominant[i] = false;
+            }
+
+            Dominant[4] = true;
+            _positive = true;
+
+            _neutral = _negative = false;
         }
+        else if (other.gameObject.CompareTag("paradox"))
+        {
+            for (int i = 0; i < Dominant.Length; i++)
+            {
+                Dominant[i] = false;
+            }
+
+            Dominant[5] = true;
+            _neutral = true;
+
+            _negative = _positive = false;
+        }
+    }
+
+    // EXIT THE TABLE
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("amnesia"))
+        {
+
+            Dominant[0] = false;
+            _negative = false;
+
+        }
+        else if (other.gameObject.CompareTag("immortal"))
+        {
+            Dominant[1] = false;
+            _positive = false;
+        }
+        else if (other.gameObject.CompareTag("trapped"))
+        {
+            Dominant[2] = false;
+            _negative = false;
+        }
+        else if (other.gameObject.CompareTag("orthogonal"))
+        {
+            Dominant[3] = false;
+            _neutral = false;
+        }
+        else if (other.gameObject.CompareTag("enlightened"))
+        {
+            Dominant[4] = false;
+            _positive = false;
+        }
+        else if (other.gameObject.CompareTag("paradox"))
+        {
+            Dominant[5] = false;
+            _neutral = false;
+
+
+        }
+
     }
 
 }
