3c256ee4304ed98bf17a369a5da330b36b6bf209
caiojmini <caiojmini@gmail.com>
2021-10-27 01:18:36 -0400

    Added hand models with animations as a prefab and hooked them up to template scene 1

---
+++ VR-Ready/Assets/Caio/Scripts/HandPresence.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public InputDeviceCharacteristics controllerCharacteristics;
+    private InputDevice targetDevice;
+
+    public GameObject handModelPrefab;
+    private GameObject spawnedHandModel;
+    private Animator handAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        if(devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+            handAnimator = spawnedHandModel.GetComponent<Animator>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHandAnimation();
+    }
+
+    private void UpdateHandAnimation()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+            handAnimator.SetFloat("trigger", triggerValue);
+        else
+            handAnimator.SetFloat("trigger", 0);
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+            handAnimator.SetFloat("grip", gripValue);
+        else
+            handAnimator.SetFloat("grip", 0);
+    }
+}
